a94b43c9b4aef148f3191a999b798ee969818490
Jeonghun Lee <cielocolo@gmail.com>
2022-04-13 23:39:40 +0900

    first commit

---
+++ .vscode/numpy-examply.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+a = np.array([1])
+b = np.array([9])
+
+c = a*b
+
+print(c)
---
+++ hello.py
@@ -0,0 +1,14 @@
+print("hello world!!")
+a =1
+b =1
+c = 1
+d = a+b+c
+print(d)
+
+import numpy as n
+a = n.array([1])
+b = n.array([5])
+c = a*b
+print(c)
+
+
